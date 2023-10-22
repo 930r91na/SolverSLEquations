de69d5d96407b4bda3524bff0568e2eea52d64c9
930r91na <georgina.zeronca@udlap.mx>
2023-10-22 14:43:48 -0600

    Float excption solved

--- main.py
+++ main.py
@@ -2,19 +2,18 @@ import numpy as np
 from sympy import Matrix, pprint
 from solution_type import *
 
+
 def main():
-    print("Menu to input a matrix A and its vector b")
-    m = int(input("Enter the number of rows/columns for the square matrix A: "))
-    A = input_matrix(m, m)
-    print("Inputted Matrix A:")
-    pprint(Matrix(A))
+    print("Menu to input a matrix mat_a and its vector b")
+    m = int(input("Enter the number of rows/columns for the square matrix mat_a: "))
+    mat_a = input_matrix(m, m)
+    print("Inputted Matrix mat_a:")
+    pprint(Matrix(mat_a))
     b = input_vector(m)
     print("Inputted Vector b:")
     pprint(Matrix(b))
-    print(get_solution_type(A, b))
+    print(get_solution_type(mat_a, b))
+
 
 if __name__ == "__main__":
     main()
-
-
-
--- solution_type.py
+++ solution_type.py
@@ -1,22 +1,40 @@
 import numpy as np
-from sympy import Matrix, pprint
+import sympy as sp
+from sympy import Matrix, pprint, nsimplify
 
-def get_solution_type(A, b):
-    detA = np.linalg.det(A)
+from solution_infinite import interpret_rref
 
-    if detA == 0:
-        augmented_A = Matrix(np.column_stack((A, b)))
-        rref_matrix = augmented_A.rref()[0]
+
+def get_solution_type(a_mat, b):
+    det_a = np.linalg.det(a_mat)
+    # Print the determinant of the matrix A
+    print ("The determinat of the matrix A is : ", det_a)
+
+    if det_a == 0:
+        augmented_a = Matrix(np.column_stack((a_mat, b)))
+        augmented_a = augmented_a.applyfunc(lambda x: nsimplify(x, rational=True))
+
+        # Print the augmented matrix
+        print("The augmented matrix is: ")
+        pprint(augmented_a)
+
+        rref_matrix = augmented_a.rref()[0]
+
+        # Print the reduced row echelon form of the augmented matrix
+        print("The reduced row echelon form of the augmented matrix is: ")
+        pprint(rref_matrix)
 
         if all([elem == 0 for elem in rref_matrix[-1, :-1]]) and rref_matrix[-1, -1] != 0:
             return "The system has no solution."
         elif all([elem == 0 for elem in rref_matrix[-1, :]]):
+            # interpret_rref(rref_matrix);
             return "The system has infinitely many solutions."
         else:
             return "The situation is ambiguous. Check the coefficient matrix and the independent term vector."
     else:
-        x = np.linalg.solve(A, b)
+        x = np.linalg.solve(a_mat, b)
         return f"The system has a unique solution: {x}"
+
 
 def input_matrix(rows, columns):
     matrix = []
@@ -33,12 +51,13 @@ def input_matrix(rows, columns):
         matrix.append(row)
     return np.array(matrix)
 
+
 def input_vector(length):
     vector = []
     for i in range(length):
         while True:
             try:
-                value = float(input(f"Enter the value for element {i + 1} of the vector: "))
+                value = float(input(f"Enter the value for element {i + 1} of the vector  b: "))
                 vector.append(value)
                 break
             except ValueError:
