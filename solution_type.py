8895ecf2429b0cf54a99c36b00b33d659f328721
930r91na <georgina.zeronca@udlap.mx>
2023-10-31 09:32:02 -0600

    Reformat of the file

--- solution_type.py
+++ solution_type.py
@@ -1,12 +1,12 @@
 import numpy as np
 from sympy import Matrix, pprint, nsimplify
 
+
 # from solution_infinite import interpret_rref
 def interpret_rref(rref_matrix):
     num_rows, num_cols = rref_matrix.shape
     solutions = []
     free_vars_positions = set(range(num_cols - 1))  # Excluding the constant column
-
 
     # Identify pivot variables and remove them from the free_vars_positions set
     for i in range(num_rows):
@@ -35,9 +35,10 @@ def interpret_rref(rref_matrix):
 
     for free_var_pos in free_vars_positions:
         solutions.append(f"x{free_var_pos + 1} is a free variable")
-    print (solutions)
+    print(solutions)
 
     return solutions
+
 
 def get_solution_type(a_mat, b):
     det_a = np.linalg.det(a_mat)
